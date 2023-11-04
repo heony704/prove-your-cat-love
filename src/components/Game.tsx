a59c6a7f418e2a8e8efef0746e9451e480ebb6c1
heony704 <heony704@gamil.com>
2023-11-04 14:29:11 +0900

    Refactor: raise GameResult component to the top
    게임 상태를 제일 상위 컴포넌트인 App 에서 관리하면서 게임 상태에 따라 Game 혹은 GameReult가 표시되도록 수정한다.
    게임 상태가 App, Game 둘 모두에서 별개로 관리되고 있기 때문에 App에 하나로 합쳤다.
    
    - 게임 상태를 ready, playing, over 세개로 변경
    - score 상태를 App에서 관리
    - useBooleanState 커스텀 훅은 사용하는 곳이 없어 삭제

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,23 +1,20 @@
 import { useEffect } from 'react';
 import { useLife } from 'src/hooks/useLife';
-import { useScore } from 'src/hooks/useScore';
-import { useBooleanState } from 'src/hooks/useBooleanState';
 import { useIntervalRandomQuiz } from 'src/hooks/useIntervalRandomQuiz';
 import { useDecreasingDelay } from 'src/hooks/useDecreasingDelay';
 import { useScoreAlarm } from 'src/hooks/useScoreAlarm';
 
 import LifeBoard from 'src/components/LifeBoard';
-import GameResult from 'src/components/GameResult';
 import ScoreBoard from 'src/components/ScoreBoard';
 
 type GameProps = {
+  score: number;
+  raiseScore: () => void;
   endGame: () => void;
 };
 
-export default function Game({ endGame }: GameProps) {
-  const { score, raiseScore, resetScore } = useScore();
+export default function Game({ score, raiseScore, endGame }: GameProps) {
   const { life, loseLife } = useLife(3);
-  const [isGameOver, gameOver] = useBooleanState(false);
 
   // 퀴즈 가속도 설정
   const { delay, clearDelay } = useDecreasingDelay(2500, 500, 25);
@@ -36,22 +33,13 @@ export default function Game({ endGame }: GameProps) {
   // 퀴즈를 3번 틀리거나 퀴즈가 3개 넘게 쌓일 경우 게임오버
   useEffect(() => {
     if (life < 1 || quizzes.length > 3) {
-      gameOver();
+      endGame();
       clearDelay();
     }
   }, [life, quizzes]);
 
   // 점수 알림
   const { ScoreAlarm } = useScoreAlarm(score);
-
-  // 게임오버 시, 게임 결과 표시
-  const onGameResultClose = () => {
-    endGame();
-    resetScore();
-  };
-  if (isGameOver) {
-    return <GameResult score={score} onClose={onGameResultClose} />;
-  }
 
   return (
     <>
