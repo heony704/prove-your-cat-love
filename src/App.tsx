5331a60b1d7e7c5de5b3a9207cd305dc55bd50ed
heony704 <heony704@gamil.com>
2023-11-04 11:32:24 +0900

    Refactor: combine MainPage to App
    MainPage 하나만 존재하기 때문에 굳이 컴포넌트로 만들어 분리할 필요가 없다고 판단했다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useBooleanState } from 'src/hooks/useBooleanState';
-import MainPage from 'src/pages/MainPage';
+import { styled } from 'styled-components';
 import Game from 'src/components/Game';
 
 export default function App() {
@@ -7,8 +7,40 @@ export default function App() {
 
   return (
     <>
-      <MainPage isStarted={isGaming} start={startGame} />
+      <Title>고양이 정말 좋아하세요?</Title>
+      <StartButton onClick={startGame} disabled={isGaming}>
+        증명하기
+      </StartButton>
       {isGaming && <Game endGame={endGame} />}
     </>
   );
 }
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  word-break: keep-all;
+`;
+
+const StartButton = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6rem 1.2rem;
+  font-size: 1rem;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+
+  &:hover {
+    border-color: #646cff;
+  }
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+
+  @media (prefers-color-scheme: light) {
+    background-color: #f9f9f9;
+  }
+`;
